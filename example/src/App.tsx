9b8d52109317970129c832e511d063d5759c1645
Claudiu HBann <123uidualcnnamreh@gmail.com>
2025-05-27 02:11:47 +0300

    feature: added the Icon component with support for feather icons

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,24 +1,32 @@
-import { StyleSheet, Text, View } from 'react-native';
-import { SvgXml } from 'react-native-svg';
-import IconsFeather from '../../src/index';
+import { FlatList, StyleSheet, Text, View } from 'react-native';
+import Icon, { IconsFeather } from '../../src/index';
 
 export default function App() {
+  const data = Array.from(IconsFeather);
+
+  const renderItem = ({ item }: { item: [string, unknown] }) => {
+    const [name] = item;
+    return (
+      <View style={styles.item}>
+        <Text>{name}</Text>
+        <Icon nameFont="feather" nameIcon={name} size={24} color="white" />
+      </View>
+    );
+  };
+
   return (
-    <View style={styles.container}>
-      {Array.from(IconsFeather).map(([name, xml]) => (
-        <View>
-          <Text>{name}: </Text>
-          <SvgXml xml={xml} width="24" height="24" />
-        </View>
-      ))}
-    </View>
+    <FlatList
+      data={data}
+      keyExtractor={([name]) => name}
+      renderItem={renderItem}
+      numColumns={7}
+    />
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  item: {
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'center',
   },
 });
